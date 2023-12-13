06df7ae33cf8d44ab7d5a53c36861ec0e4dda44a
YashVaghani999 <111616360+YashVaghani999@users.noreply.github.com>
2023-12-13 00:46:30 -0500

    COMMIT

--- APP/pages/index.js
+++ APP/pages/index.js
@@ -9,7 +9,7 @@
 *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
-*  Github Link : 
+*  Github Link : https://github.com/YashVaghani999/YVAGHANI_A6/tree/main/APP
 ********************************************************************************/ 
 
 
